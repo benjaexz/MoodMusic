82a3aa942f8f721c9500a1acb9021c9ed5caac35
Jacó <jacopimentelxd@gmail.com>
2025-09-20 14:00:25 -0400

    new

---
+++ src/main/java/com/jaco/vibetrack/VibetrackApplication.java
@@ -0,0 +1,12 @@
+package com.jaco.vibetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibetrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibetrackApplication.class, args);
+    }
+}
---
+++ src/main/java/com/jaco/vibetrack/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.jaco.vibetrack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Backend rodando!";
+    }
+}
+
